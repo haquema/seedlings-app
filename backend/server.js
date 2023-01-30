e8571c3f071b16974feff19884d1cf8a423df110
Ant-Og <hackerman.ant@gmail.com>
2023-01-30 16:42:31 +0000

    Project setup

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+require('dotenv').config(); //configures to allow you to have the env variables in the dotenv file 
+
+const app = express();
+const port = process.env.PORT || 5000; //creates express server with a defined port number
+
+app.use(cors());
+app.use(express.json()); //middleware - allows server to parse JSON when sending a receiving requests
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri);
+
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log("MongoDB database connection established successfully");
+})
+ 
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`); //starts the server on the specified port
+});
