a2e9aea9c2b615a64eaddfd2a85abffcd782aee8
henrychow <113641610+henrow5@users.noreply.github.com>
2023-01-31 12:53:12 +0000

    adds seeds file

---
+++ backend/models/seeds.js
@@ -0,0 +1,74 @@
+const mongoose = require('mongoose');
+const Plant = require('./plant.model');
+// const User = require('./user');
+
+const mongoDbUrl =
+  'mongodb+srv://Anthony-O:CC8txqzZ16v2lcra@cluster0.61xuyqe.mongodb.net/?';
+
+const connectToMongoDb = (dbUrl) => {
+  mongoose
+    .connect(dbUrl, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => console.log(`Successfully connected to MongoDB (${dbUrl})`))
+    .catch((error) => {
+      throw new Error(`Something went wrong (${error.message})`);
+    });
+};
+
+const seedDB = async (model, data) => {
+  await model.deleteMany({});
+  await model.insertMany(data);
+};
+
+const plants = [
+  {
+    img: 'http://www.tropicopia.com/house-plant/thumbnails/5556.jpg',
+    use: 'Potted plant',
+    latinName: "Dracaena deremensis 'Janet Craig'",
+    lightTolered: 'Diffuse light ( Less than 5,300 lux / 500 fc)',
+    heightAtPurchase: 91,
+    lightIdeal: 'Strong light ( 21,500 to 3,200 lux/2000 to 300 fc)',
+    widthAtPurchase: 91,
+    id: '53417c12-4824-5995-bce0-b81984ebbd1d',
+    widthPotential: 122,
+    family: 'Liliaceae',
+    heightPotential: 366,
+    description: null,
+    temperatureMax: 30,
+    watering: 'Keep moist between watering & Can dry between watering',
+    commonName: ['Janet Craig'],
+    temperatureMin: 10,
+    colorOfLeaf: ['Dark green'],
+    climate: 'Tropical',
+  },
+
+  {
+    img: 'http://www.tropicopia.com/house-plant/thumbnails/5556.jpg',
+    use: 'Potted plant',
+    latinName: "Dracaena deremensis 'Janet Craig'",
+    lightTolered: 'Diffuse light ( Less than 5,300 lux / 500 fc)',
+    heightAtPurchase: 91,
+    lightIdeal: 'Strong light ( 21,500 to 3,200 lux/2000 to 300 fc)',
+    widthAtPurchase: 91,
+    id: '53417c12-4824-5995-bce0-b81984ebbd1d',
+    widthPotential: 122,
+    family: 'Liliaceae',
+    heightPotential: 366,
+    description: null,
+    temperatureMax: 30,
+    watering: 'Keep moist between watering & Can dry between watering',
+    commonName: ['Janet Craig'],
+    temperatureMin: 10,
+    colorOfLeaf: ['Dark green'],
+    climate: 'Tropical',
+  },
+];
+
+connectToMongoDb(mongoDbUrl);
+
+seedDB(Plant, plants).then(() => {
+  console.log('Database seeded!');
+  mongoose.connection.close();
+});
