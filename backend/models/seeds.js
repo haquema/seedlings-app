f30314955bc22662e51fa24de6d7430066f0d3c9
henrychow <113641610+henrow5@users.noreply.github.com>
2023-01-31 14:21:48 +0000

    update seeds file

--- backend/models/seeds.js
+++ backend/models/seeds.js
@@ -3,7 +3,7 @@ const Plant = require('./plant.model');
 // const User = require('./user');
 
 const mongoDbUrl =
-  'mongodb+srv://Anthony-O:CC8txqzZ16v2lcra@cluster0.61xuyqe.mongodb.net/?';
+  'mongodb+srv://Anthony-O:CC8txqzZ16v2lcra@cluster0.61xuyqe.mongodb.net/seedling';
 
 const connectToMongoDb = (dbUrl) => {
   mongoose
@@ -45,23 +45,23 @@ const plants = [
   },
 
   {
-    img: 'http://www.tropicopia.com/house-plant/thumbnails/5556.jpg',
-    use: 'Potted plant',
-    latinName: "Dracaena deremensis 'Janet Craig'",
+    img: 'http://www.tropicopia.com/house-plant/thumbnails/5491.jpg',
+    use: 'Table top',
+    latinName: 'Anthurium X',
     lightTolered: 'Diffuse light ( Less than 5,300 lux / 500 fc)',
-    heightAtPurchase: 91,
+    heightAtPurchase: 21,
     lightIdeal: 'Strong light ( 21,500 to 3,200 lux/2000 to 300 fc)',
-    widthAtPurchase: 91,
-    id: '53417c12-4824-5995-bce0-b81984ebbd1d',
-    widthPotential: 122,
-    family: 'Liliaceae',
-    heightPotential: 366,
+    widthAtPurchase: 30,
+    id: '215b33f4-66d2-5601-b776-4501f2bd50b7',
+    widthPotential: 61,
+    family: 'Araceae',
+    heightPotential: 61,
     description: null,
-    temperatureMax: 30,
-    watering: 'Keep moist between watering & Can dry between watering',
-    commonName: ['Janet Craig'],
-    temperatureMin: 10,
-    colorOfLeaf: ['Dark green'],
+    temperatureMax: 22,
+    watering: 'Water when soil is half dry & Can dry between watering',
+    commonName: ['Tailflower', 'Wax flower'],
+    temperatureMin: 18,
+    colorOfLeaf: ['Medium green', 'Dark green'],
     climate: 'Tropical',
   },
 ];
