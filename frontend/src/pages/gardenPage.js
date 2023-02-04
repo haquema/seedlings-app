cad6e9176d35af5c5df7fee66bfd8089641657ec
Aimee <aimeehorwood@hotmail.com>
2023-02-04 17:16:20 +0000

    bootstrap garden page to match feed page

--- frontend/src/pages/gardenPage.js
+++ frontend/src/pages/gardenPage.js
@@ -1,5 +1,5 @@
-import React, { useEffect, useState} from "react";
-
+import React, { useEffect, useState } from "react";
+import "../components/Card.css";
 const Garden = () => {
   const [garden, setGarden] = useState([]);
   const user = window.localStorage.getItem("user_id");
@@ -16,20 +16,30 @@ const Garden = () => {
 
   return (
     <div className="garden">
+    <div className="row d-flex justify-content-center">
       {garden.map((plant, index) => (
-        <div key={plant._id || index} className="plant">
-          <h2>{plant.name}</h2>
-          <img src={plant.img} alt={plant.name} />
-          <p>{plant.description}</p>
-          <p>Known as: {plant.knownAs}</p>
-          <p>Habitat: {plant.habitat}</p>
-          <p>Toxic to: {plant.toxicTo}</p>
+        <div
+          key={plant._id || index}
+          className="card m-4 col-12 col-md-4 col-lg-4"
+        >
+          <img
+            src={plant.img}
+            className="card-img-top plant-image mx-auto d-block"
+            alt={plant.name}
+          />
+          <div className="card-body">
+            <h5 className="card-title text-center">{plant.name}</h5>
+            <p className="card-text">{plant.description}</p>
+            <p>Known as: {plant.knownAs}</p>
+            <p>Habitat: {plant.habitat}</p>
+            <p>Toxic to: {plant.toxicTo}</p>
+          </div>
         </div>
       ))}
     </div>
-  );
+  </div>
+);
 };
-
 
 
 export default Garden;
