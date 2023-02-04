d45dd7cf96f2ee771fca79db5e74b4b7bad0165a
Aimee <aimeehorwood@hotmail.com>
2023-02-04 17:35:11 +0000

    Adds conditional navbar rending so my garden patch only shows if user is logged in.  Log in + Sign up now shows if user is logged out

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,23 +1,22 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
-const Navbar = (navigate) => {
+const Navbar = () => {
   const handleLogout = () => {
-    window.localStorage.removeItem('token');
-    window.localStorage.removeItem('user_id');
-    window.localStorage.removeItem('user_name');
-    // navigate('/login');
+    window.localStorage.removeItem("token");
+    window.localStorage.removeItem("user_id");
+    window.localStorage.removeItem("user_name");
   };
 
-  // const user_id = window.localStorage.getItem('user_id');
+  const user = window.localStorage.getItem("user_id");
 
   return (
     <nav className="navbar navbar-expand-lg bg-success border me-auto p-3">
       <div className="container">
         <div className="navbar-brand ">
           <img
-            src={'/images/logo-w.png'}
+            src={"/images/logo-w.png"}
             alt="logo"
-            style={{ maxWidth: '200px' }}
+            style={{ maxWidth: "200px" }}
           />
         </div>
         <button
@@ -34,12 +33,17 @@ const Navbar = (navigate) => {
         <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
           <div className="navbar-nav ms-auto">
             <Link to="/home" className=" btn btn-success mx-1 my-2">
-              Feed
+              Home
             </Link>
-            <Link to="" className="btn btn-success mx-1 my-2">
-              My Garden Patch
-            </Link>
-            {window.localStorage.getItem('token') && (
+            {window.localStorage.getItem("token") ? (
+              <Link
+                to={`/garden/${user}`}
+                className="btn btn-success mx-1 my-2"
+              >
+                My Garden Patch
+              </Link>
+            ) : null}
+            {window.localStorage.getItem("token") ? (
               <Link
                 to="/login"
                 className="btn btn-success mx-1 my-2"
@@ -47,11 +51,15 @@ const Navbar = (navigate) => {
               >
                 Logout
               </Link>
-            )}
-            {!window.localStorage.getItem('token') && (
-              <Link to="/login" className="btn btn-success mx-1 my-2">
-                Login
-              </Link>
+            ) : (
+              <>
+                <Link to="/login" className="btn btn-success mx-1 my-2">
+                  Login
+                </Link>
+                <Link to="/signup" className="btn btn-success mx-1 my-2">
+                  Sign Up
+                </Link>
+              </>
             )}
           </div>
         </div>
