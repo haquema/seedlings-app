3ad1a48136fd767a22294c0c3a306b884c4f8a48
Aimee <aimeehorwood@hotmail.com>
2023-02-05 16:55:30 +0000

    Adds functionality so user can remove plant from their garden patch

--- backend/controllers/gardenController.js
+++ backend/controllers/gardenController.js
@@ -1,6 +1,7 @@
 const TokenGenerator = require('../models/token_generator');
 const User = require('../models/user_model');
 const Plant = require('../models/plant_model');
+const {mongoose} = require('mongoose');
 
 const profileGarden = async (req, res) => {
   const { id } = req.params;
@@ -25,22 +26,27 @@ const addPlant = async (req, res) => {
 
 //DELETE single plant from garden
 
+ 
 const deletePlantFromGarden = async (req, res) => {
-  const { id } = req.params.id;
-  const { plant_id } = req.body;
-
+  const { userid } = req.params;
+  const { plantid } = req.params;
+ 
   try {
-    const user = await User.findOne({ _id: id }, {
-      $pull: { userGardenPatch: { _id: plant_id } }
-    });
-    const token = await TokenGenerator.jsonwebtoken(req.user_id);
-
-    res.status(200).json({ message: 'OK', token: token, garden: user.userGardenPatch });
+    const user = await User.findOneAndUpdate({ _id: userid }, {
+      $pull: { userGardenPatch: plantid}
+    },{new: true});
+   
+    const token = await TokenGenerator.jsonwebtoken(req.params.userid);
+    res
+      .status(200)
+      .json({ message: "OK", token: token, garden: user.userGardenPatch });
   } catch (error) {
     console.error(error);
     res.status(500).json({ success: false, error: error.message });
   }
 };
+
+
 
 module.exports = {
   profileGarden,
