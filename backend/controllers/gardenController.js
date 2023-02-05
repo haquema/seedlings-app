f1947ee2da1c0c32d09ee61f898222b1a0028ad3
Aimee <aimeehorwood@hotmail.com>
2023-02-05 17:30:49 +0000

    Adds re-render of gardenpatch in frontend when delete from garden is clicked

--- backend/controllers/gardenController.js
+++ backend/controllers/gardenController.js
@@ -1,41 +1,50 @@
-const TokenGenerator = require('../models/token_generator');
-const User = require('../models/user_model');
-const Plant = require('../models/plant_model');
-const {mongoose} = require('mongoose');
+const TokenGenerator = require("../models/token_generator");
+const User = require("../models/user_model");
+const Plant = require("../models/plant_model");
+const { mongoose } = require("mongoose");
 
 const profileGarden = async (req, res) => {
   const { id } = req.params;
-  const user = await User.findById(id).populate('userGardenPatch');
+  const user = await User.findById(id).populate("userGardenPatch");
   const token = await TokenGenerator.jsonwebtoken(req.user_id);
 
-  res.status(200).json({ message: 'OK', token: token, garden: user.userGardenPatch });
-}
+  res
+    .status(200)
+    .json({ message: "OK", token: token, garden: user.userGardenPatch });
+};
 
 const addPlant = async (req, res) => {
   const { id } = req.params;
   const { plant_id } = req.body;
   const plantToAdd = await Plant.findById(plant_id);
-  const user = await User.findOneAndUpdate({_id: id}, {
-    $push:{"userGardenPatch":plantToAdd}
-  });
+  const user = await User.findOneAndUpdate(
+    { _id: id },
+    {
+      $push: { userGardenPatch: plantToAdd },
+    }
+  );
   const token = await TokenGenerator.jsonwebtoken(req.user_id);
 
-
-  res.status(200).json({ message: 'OK', token: token, garden: user.userGardenPatch });
-}
+  res
+    .status(200)
+    .json({ message: "OK", token: token, garden: user.userGardenPatch });
+};
 
 //DELETE single plant from garden
 
- 
 const deletePlantFromGarden = async (req, res) => {
   const { userid } = req.params;
   const { plantid } = req.params;
- 
+
   try {
-    const user = await User.findOneAndUpdate({ _id: userid }, {
-      $pull: { userGardenPatch: plantid}
-    },{new: true});
-   
+    const user = await User.findOneAndUpdate(
+      { _id: userid },
+      {
+        $pull: { userGardenPatch: plantid },
+      },
+      { new: true }
+    );
+
     const token = await TokenGenerator.jsonwebtoken(req.params.userid);
     res
       .status(200)
@@ -46,11 +55,8 @@ const deletePlantFromGarden = async (req, res) => {
   }
 };
 
-
-
 module.exports = {
   profileGarden,
   addPlant,
   deletePlantFromGarden,
-  
 };
