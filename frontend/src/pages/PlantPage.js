c3d4d8deecf2c7040cfd6ca94119a7ec779cbca4
henrychow <113641610+henrow5@users.noreply.github.com>
2023-02-08 09:56:30 +0000

    adds plant page styling

--- frontend/src/pages/PlantPage.js
+++ frontend/src/pages/PlantPage.js
@@ -1,6 +1,6 @@
-import SectionSpacer from "../components/SectionSpacer";
-import { useParams } from "react-router-dom";
-import { useEffect, useState } from "react";
+import SectionSpacer from '../components/SectionSpacer';
+import { useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 
 const PlantPage = () => {
   const { id } = useParams();
@@ -8,7 +8,7 @@ const PlantPage = () => {
 
   useEffect(() => {
     fetch(`http://localhost:5000/home/${id}`, {
-      method: "GET",
+      method: 'GET',
     })
       .then((response) => response.json())
       .then((data) => {
@@ -19,45 +19,118 @@ const PlantPage = () => {
       .catch((err) => console.error(err));
   }, []);
 
-  
+  // useEffect(() => {
+  //   const options = {
+  //     method: 'GET',
+  //     headers: {
+  //       'X-RapidAPI-Key': '50b4c8e8c5msh92331c0007a8097p127bd3jsna3504c463ed2',
+  //       'X-RapidAPI-Host': 'house-plants2.p.rapidapi.com',
+  //     },
+  //   };
+
+  //   fetch(`https://house-plants2.p.rapidapi.com/id/${_id}`, options)
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       console.log(data);
+  //       setPlant(data);
+  //       // console.log(plant['Height at purchase'].CM);
+  //     })
+  //     .catch((err) => console.error(err));
+  // }, []);
+
   return (
     <>
-      <SectionSpacer />
+      <section className="container-fluid bg-light p-5">
+        <h3 id="info-bar" className="text-center">
+          Plant Details
+        </h3>
+      </section>
 
       <section
         id="main-container"
         className="container-fluid text-center shadow-lg bg-success p-5"
       >
-        <h3 id="info-bar" className="text-center">
-          Plant Details
-        </h3>
         <div
           id="plant-details-container"
-          className="m-5 pb-4 justify-content-start bg-light rounded"
+          className="my-5 p-5 d-flex justify-content-center bg-light rounded container"
         >
-          <img src={plant.img} alt="plant" className="mt-5" />
+          <div className="card mb-3" style={{ maxWidth: '1100px' }}>
+            <div className="row g-0">
+              <div className="col-md-4">
+                <img src={plant.img} class="img-fluid" alt="..." />
+                <img src={plant.careimg} class="img-fluid" alt="..." />
+              </div>
+              <div className="col-md-8">
+                <div className="card-body">
+                  <h5 className="card-title">{plant.name}</h5>
+                  <h6 className="text-muted small">
+                    Also known as: {plant.knownAs}
+                  </h6>
+                  <p className="card-text text-start">
+                    <p>
+                      <span className="fw-bold">Description:</span>{' '}
+                      {plant.description}
+                    </p>
 
-          <p>Name: {plant.name}</p>
+                    <p>
+                      <span className="fw-bold">Toxic to: </span>
+                      {plant.toxicTo}
+                    </p>
 
-          <p>Also known as: {plant.knownAs}</p>
+                    <p>
+                      <span className="fw-bold">Minimum Temperature: </span>
+                      {plant.minTemp}°C
+                    </p>
 
-          <p>Description: {plant.description}</p>
+                    <p>
+                      <span className="fw-bold">Maximum Temperature: </span>
+                      {plant.maxTemp}°C
+                    </p>
 
-          <p>Toxic to: {plant.toxicTo}</p>
+                    <p>
+                      <span className="fw-bold">Habitat: </span>
+                      {plant.habitat}
+                    </p>
 
-          <p>Minimum Temperature: {plant.minTemp}</p>
-
-          <p>Maximum Temperature: {plant.maxTemp}</p>
-
-          <p>Habitat: {plant.habitat}</p>
-
-          <p>Pruning Instructions: {plant.pruning}</p>
+                    <p>
+                      <span className="fw-bold">Pruning Instructions: </span>
+                      {plant.pruning}
+                    </p>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-
-        <img src={plant.careimg} alt="care" />
       </section>
     </>
   );
 };
 
 export default PlantPage;
+
+{
+  /* <img src={plant.img} alt="plant" className="mt-5" />
+        
+        <p>Name: {plant.name}</p>
+
+        <p>Also known as: {plant.knownAs}</p>
+
+        <p>Description: {plant.description}</p>
+
+        <p>Toxic to: {plant.toxicTo}</p>
+
+
+        <p>Minimum Temperature: {plant.minTemp}</p>
+
+        <p>Maximum Temperature: {plant.maxTemp}</p>
+
+        <p>Habitat: {plant.habitat}</p>
+
+        <p>Pruning Instructions: {plant.pruning}</p>
+
+  
+
+   
+       <img src={plant.careimg} alt="care" /> */
+}
